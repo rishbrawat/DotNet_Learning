1efd250199b35ead97735e182d6a3c40579a8ac1
stockt0n <rishurawatx77@gmail.com>
2026-09-24 18:23:40 +0530

    Update: Minor theory changes related to memory occupation of object.

--- Classes/Basics.cs
+++ Classes/Basics.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel.Design.Serialization;
 
 /**
- * class is a user defined datatype / custom data structure, it defines what variables(data) and methods(action) an entity will have. but it does not occupy any memory until its object is created.
+ * class is a user defined datatype / custom data structure, it defines what variables(data) and methods(action) an entity will have. an instance of the class is not created untill an object is instantiated
  * 
  * class ClassName{
  *  fields
@@ -25,39 +25,41 @@ using System.ComponentModel.Design.Serialization;
     protected: accessible with class or child classes that inherit from it.
 
 */
-
-class Student
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public int Age { get; set; }
-    public string Course { get; set; }
-    public int[] Marks { get; set; }
-
-    public Student(int id, string name, int age, string course, int[] marks)
+namespace OOPS {
+    class Student
     {
-        this.Id = id;
-        this.Name = name;
-        this.Age = age;
-        this.Course = course;
-        this.Marks = marks;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Course { get; set; }
+        public int[] Marks { get; set; }
+
+        public Student(int id, string name, int age, string course, int[] marks)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+            this.Course = course;
+            this.Marks = marks;
+        }
+
+        public void printDetails()
+        {
+            Console.WriteLine($" Name: {this.Name}\n ID: {this.Id}\n Age: {this.Age}\n Course: {this.Course}\n");
+        }
+
     }
 
-    public void printDetails()
+    public class MainProgram
     {
-        Console.WriteLine($" Name: {this.Name}\n ID: {this.Id}\n Age: {this.Age}\n Course: {this.Course}\n");
+        static void Main()
+        {
+            Student std1 = new Student(1, "Rishabh Rawat", 23, "BTech CSE", new int[] { 96, 97, 95, 99, 97 });
+            Student std2 = new Student(2, "Ankush Chauhan", 25, "BTech CSE", new int[] {61, 45, 41, 62, 0});
+
+            std1.printDetails();
+            std2.printDetails();
+        }
     }
 
-}
-
-public class MainProgram
-{
-    static void Main()
-    {
-        Student std1 = new Student(1, "Rishabh Rawat", 23, "BTech CSE", new int[] { 96, 97, 95, 99, 97 });
-        Student std2 = new Student(2, "Ankush Chauhan", 25, "BTech CSE", new int[] {61, 45, 41, 62, 0});
-
-        std1.printDetails();
-        std2.printDetails();
-    }
 }
